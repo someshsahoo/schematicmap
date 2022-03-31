e5f7e06766e6ab45ea34ff6d3391fb707bba6bac
Somesh Sahoo <someshsahoo23@gmail.com>
2022-03-31 21:36:30 +0530

    Errors in Scolly. Need to debug

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,6 @@
-// Import all plugins
+import enterViewScrolly from "../scrolly/enterViewscrolly"
+
 import * as bootstrap from 'bootstrap';
-console.log('Hello from the console world!');
+console.log ("Random Message");
+
+enterViewScrolly ("#map-scrolly")
